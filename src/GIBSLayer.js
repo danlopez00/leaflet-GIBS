a621995dd3f14730fb9f03aa56fc429be8db7a2f
vincentsarago <vincent.sarago@gmail.com>
2015-12-16 19:17:12 -0500

    Add SUOMI NPP layers Add non-minified version of GIBSMetadata.js Add minified version of GIBSLayer.js

--- src/GIBSLayer.js
+++ src/GIBSLayer.js
@@ -1,10 +1,11 @@
 ﻿(function() {
     var s2 = function(num) { return num < 10 ? '0' + num : num; },
         gibsAttribution = '<a href="https://earthdata.nasa.gov/gibs">NASA EOSDIS GIBS</a>';
-    
+
     var getGibsURL = function(info, date, x, y, z) {
-        var dateStr = info.date ? date.getUTCFullYear() + '-' + s2(date.getUTCMonth() + 1) + '-' + s2(date.getUTCDate()) : "0";
-        
+        //var dateStr = info.date ? date.getUTCFullYear() + '-' + s2(date.getUTCMonth() + 1) + '-' + s2(date.getUTCDate()) : "0";
+        var dateStr = info.date ? date : "0";
+
         return L.Util.template(info.template, {
             Time: dateStr,
             TileMatrixSet: 'GoogleMapsCompatible_Level' + info.zoom,
@@ -13,7 +14,7 @@
             TileCol: x
         });
     };
-    
+
     var GIBSLayerImage = L.TileLayer.extend({
         initialize: function(gibsID, options) {
             this._layerInfo = L.GIBS_LAYERS[gibsID];
@@ -21,16 +22,16 @@
             options.maxZoom = this._layerInfo.zoom;
             options.attribution = gibsAttribution;
             this._date = options.date || null;
-            
+
             L.Util.setOptions(this, options);
-            
+
             L.TileLayer.prototype.initialize.call(this, this._layerInfo.template, options);
         },
-        
+
         getTileUrl: function(tilePoint){
             return getGibsURL(this._layerInfo, this._date, tilePoint.x, tilePoint.y, tilePoint.z);
         },
-        
+
         setDate: function(newDate) {
             if (this._layerInfo.date) {
                 this._date = newDate;
@@ -38,32 +39,32 @@
             }
             return this;
         },
-        
+
         isTemporal: function() {
             return this._layerInfo.date;
         }
-        
+
         //setTransparent: function(isTransparent) { return this; } //to ensure the same methods for both classes
     });
 
     var GIBSLayerCanvas = L.TileLayer.Canvas.extend({
         initialize: function(layerName, options) {
-        
+
             L.Util.setOptions(this, {
                 async: true,
                 attribution: gibsAttribution
             });
             L.Util.setOptions(this, options);
-            
+
             this._date = this.options.date || null;
             this._layerInfo = L.GIBS_LAYERS[layerName];
-            
+
             if (!this._layerInfo) {
                 throw "Unknown GIBS layer name";
             }
-            
+
             L.Util.setOptions(this, {maxZoom: this._layerInfo.zoom});
-            
+
             this._maskInfo = null;
             if (layerName.indexOf('Terra') !== -1) {
                 this._maskInfo = L.GIBS_MASKS['MODIS_Terra_Data_No_Data'];
@@ -71,7 +72,7 @@
                 this._maskInfo = L.GIBS_MASKS['MODIS_Aqua_Data_No_Data'];
             }
         },
-        
+
         setDate: function(newDate) {
             if (this._layerInfo.date) {
                 this._date = newDate;
@@ -79,13 +80,13 @@
             }
             return this;
         },
-        
+
         setTransparent: function(isTransparent) {
             this.options.transparent = isTransparent;
             this._map && this.redraw();
             return this;
         },
-        
+
         _loadImage: function(url, onLoaded, onError) {
             var img = new Image();
             img.onload = onLoaded.bind(null, img);
@@ -93,37 +94,37 @@
             img.crossOrigin = "anonymous";
             img.src = url;
         },
-                
+
         drawTile: function(canvas, tilePoint, zoom) {
             var hasMask = this._maskInfo && this.options.transparent,
                 mainImg,
                 maskImg,
                 _this = this;
-            
+
             if (!this._date) {
                 return;
             }
-            
-            
+
+
             var tryToProcess = function() {
                 if (mainImg && (maskImg || !hasMask)) {
                     if (mainImg.width !== 256 || (hasMask && maskImg.width !== 256)) {
                         _this.tileDrawn(canvas);
                         return;
                     }
-                    
+
                     var mainCtx = canvas.getContext('2d');
                     mainCtx.drawImage(mainImg, 0, 0);
-                    
+
                     if (hasMask) {
                         var mainData = mainCtx.getImageData(0, 0, 256, 256);
-                        
+
                         var maskCanvas = document.createElement('canvas');
                         maskCanvas.width = maskCanvas.height = 256;
-                        
+
                         var maskCtx = maskCanvas.getContext('2d');
                         maskCtx.drawImage(maskImg, 0, 0);
-                        
+
                         var maskPixels = maskCtx.getImageData(0, 0, 256, 256).data,
                             pixels = mainData.data;
                         for (var p = 0; p < maskPixels.length; p += 4) {
@@ -131,23 +132,23 @@
                                 pixels[p+3] = 0;
                             }
                         }
-                        
+
                         mainCtx.putImageData(mainData, 0, 0);
                     }
-                    
+
                     _this.tileDrawn(canvas);
                 }
             }
-            
+
             this._adjustTilePoint(tilePoint);
-            
+
             var mainSrc = getGibsURL(this._layerInfo, this._date, tilePoint.x, tilePoint.y, zoom);
-            
+
             this._loadImage(mainSrc, function(img) {
                 mainImg = img;
                 tryToProcess();
             }, this.tileDrawn.bind(this, canvas));
-            
+
             if (hasMask) {
                 var maskSrc = getGibsURL(this._maskInfo, this._date, tilePoint.x, tilePoint.y, zoom);
                 this._loadImage(maskSrc, function(img) {
@@ -156,22 +157,22 @@
                 }, this.tileDrawn.bind(this, canvas));
             }
         },
-        
+
         isTemporal: function() {
             return this._layerInfo.date;
         }
     })
-    
+
     L.GIBSLayer = function(gibsID, options) {
         var layerInfo = L.GIBS_LAYERS[gibsID];
-            
+
         if (!layerInfo) {
             throw "Unknown GIBS layer name";
         }
-        
-        var needMask = layerInfo.date && 'transparent' in options && /jpg$/.test(layerInfo.template) && 
+
+        var needMask = layerInfo.date && 'transparent' in options && /jpg$/.test(layerInfo.template) &&
                 (gibsID.indexOf('Terra') !== -1 || gibsID.indexOf('Aqua') !== -1);
-                
+
         return needMask ? new GIBSLayerCanvas(gibsID, options) : new GIBSLayerImage(gibsID, options);
     }
 })();
